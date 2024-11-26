5ba4bd47cbbc09663bda2a57d3430ff926b35def
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2024-11-26 15:24:35 +0700

    Add more account column

---
+++ DrugWarehouseManagement.Repository/Models/SeedData.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugWarehouseManagement.Repository.Models
+{
+    public class SeedData
+    {
+        private readonly ModelBuilder modelBuilder;
+        public SeedData(ModelBuilder modelBuilder)
+        {
+            this.modelBuilder = modelBuilder;
+        }
+
+        public void Seed()
+        {
+            modelBuilder.Entity<Role>().HasData(
+                new Role
+                {
+                    RoleId = 1,
+                    RoleName = "Admin"
+                },
+                new Role
+                {
+                    RoleId = 2,
+                    RoleName = "Inventory Manager"
+                },
+                new Role
+                {
+                    RoleId = 3,
+                    RoleName = "Inventory Staff"
+                });
+        }
+    }
+}
