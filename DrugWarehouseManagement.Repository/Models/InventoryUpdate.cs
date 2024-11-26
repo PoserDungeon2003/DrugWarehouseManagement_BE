03eb78957c82c710ebb551d2bb3f90e6c9212cbb
kong18 <thanh2000cong20000@gmail.com>
2024-11-26 13:09:38 +0700

    first ib ob

---
+++ DrugWarehouseManagement.Repository/Models/InventoryUpdate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugWarehouseManagement.Repository.Models
+{
+    public class InventoryUpdate
+    {
+        public int Quantity { get; set; } // Updated quantity of the product in stock
+        public decimal CostPrice { get; set; } // Cost price of the received goods
+        public DateTime? ExpirationDate { get; set; } // Expiration date of the received drugs
+        public string Location { get; set; } // Storage location
+    }
+}
