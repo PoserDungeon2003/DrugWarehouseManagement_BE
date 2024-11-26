bcdf3dae4b0f4c087ed66375adc22090cd760d17
sonhohuu <hhson365@gmail.com>
2024-11-26 20:12:50 +0700

    feat(model): add Product and Drug model + seed data

---
+++ DrugWarehouseManagement.Repository/Models/Drug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrugWarehouseManagement.Repository.Models
+{
+    public class Drug : BaseModel
+    {
+        [Key]
+        public Guid DrugId { get; set; } = Guid.NewGuid();
+        public Guid? ProductId { get; set; }
+        public virtual Product Product { get; set; } = null!;
+        public string TemperatureRange { get; set; } = null!;
+        public string HumidityRange { get; set; } = null!;
+        public bool IsLightSensitive { get; set; } = false;
+    }
+}
