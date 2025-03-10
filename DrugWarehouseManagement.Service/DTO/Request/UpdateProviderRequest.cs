6f2952ec7090309f4b73f14d26f328ea5d561fae
kong18 <thanh2000cong20000@gmail.com>
2025-03-10 15:23:36 +0700

    code

---
+++ DrugWarehouseManagement.Service/DTO/Request/UpdateProviderRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace DrugWarehouseManagement.Service.DTO.Request
+{
+    public class UpdateProviderRequest
+    {
+        public string? ProviderName { get; set; }
+        public string? Address { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? TaxCode { get; set; }
+        public string? Nationality { get; set; }
+        public string? Email { get; set; }
+        public string? DocumentNumber { get; set; }
+        public DateOnly? DocumentIssueDate { get; set; }
+    }
+}
