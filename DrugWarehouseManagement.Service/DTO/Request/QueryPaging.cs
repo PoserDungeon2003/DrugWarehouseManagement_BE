fc38ed4e617c1069add32fd5c6127593fd66ed27
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2025-04-01 01:15:58 +0700

    Fix bug

--- DrugWarehouseManagement.Service/DTO/Request/QueryPaging.cs
+++ DrugWarehouseManagement.Service/DTO/Request/QueryPaging.cs
@@ -2,7 +2,7 @@
 
 namespace DrugWarehouseManagement.Service.DTO.Request
 {
-    public abstract class QueryPaging
+    public class QueryPaging
     {
         public int Page { get; set; } = 1;
         public int PageSize { get; set; } = 10;
