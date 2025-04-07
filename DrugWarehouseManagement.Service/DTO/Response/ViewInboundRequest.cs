6a5dee4d0028e2d5e97bde72c05a5b380a3c050b
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2025-04-07 11:38:10 +0700

    Add product id

--- DrugWarehouseManagement.Service/DTO/Response/ViewInboundRequest.cs
+++ DrugWarehouseManagement.Service/DTO/Response/ViewInboundRequest.cs
@@ -23,7 +23,8 @@ namespace DrugWarehouseManagement.Service.DTO.Response
 
     public class InboundRequestDetailResponse
     {
-        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
         public int Quantity { get; set; }
         public decimal UnitPrice { get; set; }
         public decimal TotalPrice { get; set; }
