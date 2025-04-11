e91bc4e317a99e71333914c1475645bc0302d943
Ngo Quang Thang <ngoquangthang2003@gmail.com>
2025-04-11 23:10:46 +0700

    Add get category by id api

--- DrugWarehouseManagement.Service/Interface/ICategoriesService.cs
+++ DrugWarehouseManagement.Service/Interface/ICategoriesService.cs
@@ -16,5 +16,6 @@ namespace DrugWarehouseManagement.Service.Interface
         public Task<BaseResponse> UpdateCategory(UpdateCategoryRequest updateCategoryRequest);
         public Task<BaseResponse> DeleteCategory(int categoryId);
         public Task<BaseResponse> ActiveCategory(int categoryId);
+        public Task<ViewCategories> GetCategoryById(int categoryId);
     }
 }
