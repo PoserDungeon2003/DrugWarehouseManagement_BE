2fca6fffda1cb0125162ca84bbb3528b9edd1596
kong18 <thanh2000cong20000@gmail.com>
2025-04-30 13:18:53 +0700

    fix authorize

--- DrugWarehouseManagement.API/Controllers/OutboundController.cs
+++ DrugWarehouseManagement.API/Controllers/OutboundController.cs
@@ -132,7 +132,6 @@ namespace DrugWarehouseManagement.API.Controllers
            
         }
         [HttpGet("export/{id}")]
-        [Authorize(Roles = "Inventory Manager, Sale Admin")]
         public async Task<IActionResult> ExportOutboundInvoice(int id)
         {
             try
